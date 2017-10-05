0d9d6d10719cdd004f4d185cc5a114091da855b6
geoffcraz <geoffcraz@gmail.com>
2017-10-05 18:35:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,12 +4,7 @@
 }
 
 */
-function updatePlaylist(playlist, artist, song) {
-  // Remember, because one of this function's arguments
-  // is `playlist`, the `playlist` _inside_ the function
-  // is not the same as the top-level `playlist` outside
-  // the function
-  playlist[artist] = song
-
+updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
   return playlist
 }
