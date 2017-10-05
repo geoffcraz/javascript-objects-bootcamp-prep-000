b04fde2c59d29ef59922134d8185e2e29d526386
geoffcraz <geoffcraz@gmail.com>
2017-10-05 18:23:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
 var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[artistName] = songTitle
-}
+  var updatedPlaylist = playlist[artistName] = songTitle
+  return updatedPlaylist
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   return playlist
 }
+
